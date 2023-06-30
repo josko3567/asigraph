e031d9579e99ed00216069350a102c5cba96cace
josko3567 <jkrianovic123@gmail.com>
2023-06-30 23:47:39 +0200

    Forgor to add the files

---
+++ asigraph.h
@@ -0,0 +1,56 @@
+/** INSCRIBING: *******************************************
+ * 
+ *  > PROJECT: @b ASigraph
+ * 
+ *  > AUTHOR:
+ *      > NAME: Joško K.
+ *      > EMAIL: jkriza02@fesb.hr
+ * 
+ *  > DOC: 30.6.2023.
+ * 
+ ** INTRODUCTION: *****************************************
+ * 
+ *   ASigraph is a cross-platform console/terminal 
+ *   "graphics" library. This will be a library mostly for
+ *   creating GUI's but making console games is plausible 
+ *   to.
+ * 
+ ** COMPATIBILITY: ****************************************
+ *   
+ *  > C:                                                   
+ *   This library is written in compliance to the limited  
+ *   functionality at the disposal of the GNU C99 standard.    
+ *                                                       
+ *                                                       
+ *  > POSIX:                                               
+ *   This libraries creator has yet to inscribe the minimum
+ *   POSIX compatibility version that this library requires.
+ *   In its absence the user of the library is free to use 
+ *   any compatibility version of the POSIX standard that is
+ *   deemed to work through trial and error.               
+ * 
+ ** LICENSE: @c LGPLv2 ************************************
+ * 
+ *   This file is part of Project ASigraph, a library for 
+ *   cross-platform console/terminal graphics.
+ *   Copyright (C) 2023 Joško Križanović
+ *   
+ *   This library is free software; you can redistribute 
+ *   it and/or modify it under the terms of the GNU Lesser 
+ *   General Public License as published by the Free 
+ *   Software Foundation; either version 2.1 of the 
+ *   License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will 
+ *   be useful, but WITHOUT ANY WARRANTY; without even the 
+ *   implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ *   PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ *   License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser 
+ *   General Public License along with this library; if 
+ *   not, write to the Free Software Foundation, Inc., 
+ *   51 Franklin Street, Fifth Floor,  Boston, 
+ *   MA  02110-1301  USA
+ * 
+ **********************************************************/
